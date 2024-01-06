79f5faae9bd4c54444f50a1809922dde14e566f9
alfityah18 <155342492+alfityah18@users.noreply.github.com>
2024-01-06 11:38:45 +0800

    Update

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mode-switch').addEventListener('change', function () {
+    document.body.classList.toggle('night-mode', this.checked);
+    document.getElementById('mode-label').innerText = this.checked ? 'Night Mode' : 'Day Mode';
+});
