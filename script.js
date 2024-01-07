7571d24290b3b2cd3e134811063711d8a7f24e78
alfityah18 <155342492+alfityah18@users.noreply.github.com>
2024-01-07 12:00:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ document.getElementById('mode-switch').addEventListener('change', function () {
     document.getElementById('mode-label').innerText = this.checked ? '🌙' : '☀️';
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Get the image element
-  var image = document.querySelector('.enlargeable-image img');
-
-  // Add a click event listener to the image
-  image.addEventListener('click', function () {
-    // Add the 'enlarged' class to the image
-    image.classList.add('enlarged');
-  });
-});
+  function enlargeImage(image) {
+    image.classList.toggle('enlarged');
+  }
